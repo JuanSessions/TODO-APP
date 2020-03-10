a3e74848f5f8d6cea3be37e46d8e0534b37f31f8
JuanSessions <juanperusin@yahoo.es>
2020-03-10 10:30:57 +0100

    Updates

---
+++ precache-manifest.330a12d024c9535163ef898cd79658cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e6a64e7dacb37f8f8712b858bd382a1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "07e4fd5e8bef387113bb",
+    "url": "./static/css/main.2ae354f5.chunk.css"
+  },
+  {
+    "revision": "7bb3b3118b0f650fa947",
+    "url": "./static/js/2.9b51bb38.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "./static/js/2.9b51bb38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07e4fd5e8bef387113bb",
+    "url": "./static/js/main.5334830f.chunk.js"
+  },
+  {
+    "revision": "7d2d16499b417a029b08",
+    "url": "./static/js/runtime-main.dc7f9edc.js"
+  }
+]);
